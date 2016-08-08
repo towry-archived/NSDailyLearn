a6a4baae2161c96fc1d14323657640cff864c425
towry <tovvry@gmail.com>
2016-08-08 17:00:36 +0800

    Add scale image view

--- NSImage-View/DrawImagePortion/MainView.m
+++ NSImage-View/DrawImagePortion/MainView.m
@@ -21,13 +21,19 @@
 	[[NSColor blackColor] setFill];
 	NSRectFill(dirtyRect);
 	
-//	[self drawOurImage];
-	if ([self respondsToSelector:@selector(drawOurImage)]) {
-		[self performSelector:@selector(drawOurImage) withObject:self];
+//	[self drawNormalImage];
+	if ([self respondsToSelector:@selector(drawNormalImage)]) {
+		[self performSelector:@selector(drawNormalImage) withObject:self];
 	}
+	
+	// draw the scaled image
+	[self drawImageWithScale];
+	
+	// draw the third image
+	[self drawThirdImage];
 }
 
-- (void)drawOurImage {
+- (void)drawNormalImage {
 	NSCompositingOperation op = NSCompositeSourceOver;
 	NSImage *image = [NSImage imageNamed:@"pacman.png"];
 	
@@ -36,6 +42,42 @@
 	NSPoint origin = NSMakePoint(10, 48);
 	NSRect srcRect = NSMakeRect(2, imageSize.height - 50, 182, 47);
 	CGFloat delta = 1.0;
+	
+	// draw the image
+	[image drawAtPoint:origin fromRect:srcRect operation:op fraction:delta];
+}
+
+- (void)drawImageWithScale {
+	NSAffineTransform *transform = [NSAffineTransform transform];
+	[transform scaleBy:0.5];
+	[transform concat];
+	
+	NSCompositingOperation op = NSCompositeSourceOver;
+	NSImage *image = [NSImage imageNamed:@"pacman.png"];
+	
+	NSSize imageSize = image.size;
+	NSPoint origin = NSMakePoint(10 * 2, (48 * 2 + 10) * 2);
+	NSRect srcRect = NSMakeRect(2, imageSize.height - 50, 182, 47);
+
+	CGFloat delta = 1;
+	
+	// draw the image
+	[image drawAtPoint:origin fromRect:srcRect operation:op fraction:delta];
+	
+	// after draw the image
+	[transform invert];
+	[transform concat];
+}
+
+- (void)drawThirdImage {
+	NSCompositingOperation op = NSCompositeSourceOver;
+	NSImage *image = [NSImage imageNamed:@"pacman.png"];
+	
+	NSSize imageSize = image.size;
+	
+	NSPoint origin = NSMakePoint(10, 48 * 3);
+	NSRect srcRect = NSMakeRect(2, imageSize.height - 50, 182, 47);
+	CGFloat delta = .5;
 	
 	// draw the image
 	[image drawAtPoint:origin fromRect:srcRect operation:op fraction:delta];
